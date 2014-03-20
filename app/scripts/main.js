e98359dcf84e129eeebc63431f3957a419da7eec
neven <neven@burza.hr>
2014-03-20 19:38:38 +0100

    novo

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -41,5 +41,47 @@ $(document).ready(function() {
 
     });
 
+    function viewport() {
+        var e = window, a = 'inner';
+        if (!('innerWidth' in window )) {
+            a = 'client';
+            e = document.documentElement || document.body;
+        }
+        return {
+            width : e[ a+'Width' ],
+            height : e[ a+'Height' ]
+        };
+    }
+    function updateVw(){
+
+        var vw = (viewport().width/100);
+        if(viewport().width < 1270){
+            vw = (viewport().width/70);
+        }
+
+        if(viewport().width < 800){
+            vw = (viewport().width/60);
+        }
+
+
+        if(vw > 16) {
+            vw = 16;
+        }
+
+
+        if (viewport().width <= 602) {
+            var freeze = parseFloat($('html').attr('style').match(/[+-]?\d+\.\d+/g)) || 13;
+            vw = freeze + 0.1;
+        }
+
+        jQuery('html').css({
+            'font-size' : vw + 'px'
+        });
+    }
+
+
+    jQuery(window).resize(function(){
+        updateVw();
+    }).trigger('resize');
 
 });
